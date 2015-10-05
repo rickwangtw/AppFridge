180fa91e2b51b911669b08fae8cb2ca09a056475
Rick Wang <rickwangtw@hotmail.com>
2015-10-05 12:25:30 +0800

    GeneralPreferenceFragment: launch dialog to go to accessibility settings if the service is not configured correctly

--- app/src/main/java/com/mysticwind/disabledappmanager/ui/activity/settings/GeneralPreferenceFragment.java
+++ app/src/main/java/com/mysticwind/disabledappmanager/ui/activity/settings/GeneralPreferenceFragment.java
@@ -1,11 +1,17 @@
 package com.mysticwind.disabledappmanager.ui.activity.settings;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.provider.Settings;
 
+import com.google.common.base.Splitter;
 import com.mysticwind.disabledappmanager.R;
 import com.mysticwind.disabledappmanager.config.DaggerGeneralPreferenceFragmentComponent;
 import com.mysticwind.disabledappmanager.config.GeneralPreferenceFragmentComponent;
@@ -13,6 +19,7 @@ import com.mysticwind.disabledappmanager.config.GeneralPreferenceFragmentModule;
 import com.mysticwind.disabledappmanager.domain.config.AutoDisablingConfig;
 import com.mysticwind.disabledappmanager.domain.config.AutoDisablingConfigService;
 import com.mysticwind.disabledappmanager.domain.config.AutoDisablingConfig_;
+import com.mysticwind.disabledappmanager.service.AppSwitchDetectionService;
 
 import org.androidannotations.annotations.AfterPreferences;
 import org.androidannotations.annotations.EFragment;
@@ -30,6 +37,7 @@ public class GeneralPreferenceFragment extends PreferenceFragment {
 
     private GeneralPreferenceFragmentComponent component;
     private AutoDisablingConfigService autoDisablingConfigService;
+    private Dialog goToAccessibilitySettingsDialog;
 
     @Pref
     AutoDisablingConfig_ autoDisablingConfig;
@@ -45,6 +53,17 @@ public class GeneralPreferenceFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         getPreferenceManager().setSharedPreferencesName(AutoDisablingConfig.class.getSimpleName());
+
+        goToAccessibilitySettingsDialog = new AlertDialog.Builder(getActivity())
+                .setTitle("Please enable the service in accessibility settings")
+                .setMessage("The service must be enabled to detect the switching of apps!")
+                .setPositiveButton("Go to Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startAccessibilitySettings();
+                    }
+                })
+                .setNegativeButton("Cancel", null).create();
     }
 
     @AfterPreferences
@@ -61,13 +80,55 @@ public class GeneralPreferenceFragment extends PreferenceFragment {
         autoDisableTimeoutPreference.setValue(
                 String.valueOf(autoDisablingConfigService.getAutoDisablingTimeoutInSeconds()));
 
+        if (autoDisablingConfigService.isAutoDisablingOn()
+                && !isAppSwitchDetectionAccessibilitySettingsEnabled()) {
+            goToAccessibilitySettingsDialog.show();
+        }
+
         enableAppLaunchAutoDisablePreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
-                autoDisablingConfigService.setAutoDisablingState((Boolean)newValue);
+                boolean enable = (Boolean) newValue;
+                if (enable && !isAppSwitchDetectionAccessibilitySettingsEnabled()) {
+                    goToAccessibilitySettingsDialog.show();
+                    return false;
+                }
+                autoDisablingConfigService.setAutoDisablingState(enable);
                 return true;
             }
         });
+    }
+
+    private boolean isAppSwitchDetectionAccessibilitySettingsEnabled() {
+        final String appSwitchDetectionServiceFullName =
+                getActivity().getPackageName() + "/" + AppSwitchDetectionService.class.getCanonicalName();
+        try {
+            if (Settings.Secure.getInt(
+                    getActivity().getContentResolver(),
+                    Settings.Secure.ACCESSIBILITY_ENABLED) != 1) {
+                return false;
+            }
+        } catch (Settings.SettingNotFoundException e) {
+            log.warn("Error finding accessibility settings", e);
+            return false;
+        }
+        String allEnabledServicesDelimitedWithColon = Settings.Secure.getString(
+                getActivity().getContentResolver(),
+                Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+
+        Iterable<String> enabledServiceNames = Splitter.on(':').split(allEnabledServicesDelimitedWithColon);
+
+        for (String enabledServiceName : enabledServiceNames) {
+            if (appSwitchDetectionServiceFullName.equals(enabledServiceName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void startAccessibilitySettings() {
+        startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
     }
 
     @PreferenceChange(R.string.pref_key_auto_disable_timeout)
